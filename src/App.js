f8081981766afc8fd76f5b5fcc5b1647ba5bf48c
anti-15 <taiki1151001@icloud.com>
2023-09-08 23:58:57 +0900

    fix: Counter.jsxのUIを変更

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import Counter from "./components/Counter";
 function App() {
+  const [countList, setCountList] = useState([<Counter key={0} />]);
+
+  const handleAddCount = () => {
+    setCountList([...countList, <Counter key={countList.length} />]);
+    console.log(countList);
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="p-5 mx-auto max-w-5xl md:grid grid-cols-2 ">
+        {countList}
+        <button
+          className="mt-4  h-64 mx-auto block"
+          onClick={handleAddCount}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <svg
+            className="h-20 w-20 text-gray-400"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            {" "}
+            <path stroke="none" d="M0 0h24v24H0z" />{" "}
+            <circle cx="12" cy="12" r="9" />{" "}
+            <line x1="9" y1="12" x2="15" y2="12" />{" "}
+            <line x1="12" y1="9" x2="12" y2="15" />
+          </svg>
+        </button>
+      </div>
+    </>
   );
 }
 
